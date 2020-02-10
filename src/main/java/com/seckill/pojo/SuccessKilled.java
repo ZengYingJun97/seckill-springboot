dd9b14b9455ce460933692bdbf4cb4e5f8929054
zengyingjun <13692916769@163.com>
2020-02-10 22:39:50 +0800

    test

--- src/main/java/com/seckill/pojo/SuccessKilled.java
+++ src/main/java/com/seckill/pojo/SuccessKilled.java
@@ -1,9 +1,61 @@
 package com.seckill.pojo;
 
+import java.util.Date;
+
 /**
  * SuccessKilled
  * @author handsome
  * @date 2020年 02月10日 12:12:11
  */
 public class SuccessKilled {
+
+	private long seckillId;
+
+	private long userPhone;
+
+	private short state;
+
+	private Date createTime;
+
+	private Seckill seckill;
+
+	public long getSeckillId() {
+		return seckillId;
+	}
+
+	public void setSeckillId(long seckillId) {
+		this.seckillId = seckillId;
+	}
+
+	public long getUserPhone() {
+		return userPhone;
+	}
+
+	public void setUserPhone(long userPhone) {
+		this.userPhone = userPhone;
+	}
+
+	public short getState() {
+		return state;
+	}
+
+	public void setState(short state) {
+		this.state = state;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public Seckill getSeckill() {
+		return seckill;
+	}
+
+	public void setSeckill(Seckill seckill) {
+		this.seckill = seckill;
+	}
 }
