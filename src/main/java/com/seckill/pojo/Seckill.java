034e116e1d44370477d36aedde0118d759274775
zengyingjun <13692916769@163.com>
2020-02-10 21:01:08 +0800

    pojo

---
+++ src/main/java/com/seckill/pojo/Seckill.java
@@ -0,0 +1,71 @@
+package com.seckill.pojo;
+
+import java.util.Date;
+
+/**
+ * Seckill
+ * @author handsome
+ * @date 2020年 02月10日 12:09:41
+ */
+public class Seckill {
+
+	private long seckillId;
+
+	private String name;
+
+	private int number;
+
+	private Date createTime;
+
+	private Date startTime;
+
+	private Date endTime;
+
+	public long getSeckillId() {
+		return seckillId;
+	}
+
+	public void setSeckillId(long seckillId) {
+		this.seckillId = seckillId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+}
---
+++ src/main/java/com/seckill/pojo/SuccessKilled.java
@@ -0,0 +1,9 @@
+package com.seckill.pojo;
+
+/**
+ * SuccessKilled
+ * @author handsome
+ * @date 2020年 02月10日 12:12:11
+ */
+public class SuccessKilled {
+}
